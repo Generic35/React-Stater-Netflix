5f968301717773120027f746f0addbb476e05f50
elamonta <ricklamontagne@gmail.com>
2018-06-28 14:46:35 -0400

    Add details component

--- js/Details.jsx
+++ js/Details.jsx
@@ -2,12 +2,33 @@
 
 import React from "react";
 
-const Details = props => (
-  <div className="details">
-    
-    <pre><code>{JSON.stringify(props, null, 4)} </code></pre>
-    
-  </div>
-);
+const Details = (props: { show: Show }) => {
+  const { title, description, poster, trailer, year } = props.show;
+  return (
+    <div className="details">
+      <header>
+        <h1>svideo</h1>
+      </header>
+      <section>
+        <h2>{title}</h2>
+        <h2>({year})</h2>
+        <img
+          src={`/public/img/posters/${poster}`}
+          alt={`Poster for ${title}`}
+        />
+        <p>{description}</p>
+      </section>
+      <div>
+        <iframe
+          src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
+          frameBorder="0"
+          allowFullScreen
+          title={`Trailer for ${title}`}
+        />
+      </div>
+
+    </div>
+  );
+};
 
 export default Details;
