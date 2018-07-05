d15a93b7f4b29d7f8fe781f0378fdfdb454feaf7
elamonta <ricklamontagne@gmail.com>
2018-07-05 07:48:06 -0400

    connect the landing page to redux

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,14 +1,26 @@
 // @flow
 
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { setSearchTerm } from "./actionCreators";
 
-const Landing = () => (
+const Landing = (props: { searchTerm: string, handleSearchTermChange: Function }) => (
   <div className="landing">
-    <h1>sjometing else</h1>
-    <input type="text" placeholder="Search" />
+    <h1>svideo</h1>
+    <input type="text" value={props.searchTerm} onChange={props.handleSearchTermChange} placeholder="Search" />
     <Link to="/search">or Browse All</Link>
   </div>
 );
 
-export default Landing;
+const mapStateToProps = state => ({
+  searchTerm: state.searchTerm
+});
+
+const mapDispatchToProps = (dispatch: Function) => ({
+  handleSearchTermChange: event => {
+    dispatch(setSearchTerm(event.target.value));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Landing);
