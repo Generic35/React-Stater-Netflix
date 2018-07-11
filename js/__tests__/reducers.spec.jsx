5f5de3a4974da3fb9f325eec6c37475ade5340ac
Eric Lamontagne <ricklamontagne@gmail.com>
2018-07-11 09:34:04 -0400

    Add reducer test
    generated from dev tools

---
+++ js/__tests__/reducers.spec.jsx
@@ -0,0 +1,6 @@
+import reducers from '../reducers';
+
+test('SET_SEARCH_TERM', () => {
+  const state = reducers({searchTerm:'',apiData:{}}, {type:'SET_SEARCH_TERM',payload:'black'});
+  expect(state).toEqual({searchTerm:'black',apiData:{}});
+});
